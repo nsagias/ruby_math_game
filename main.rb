76042aabb5e3b2dc93c83226e6824d8a75f3395f
Nick <nsagias@gmail.com>
2021-11-10 16:56:15 -0500

    Backing up

--- main.rb
+++ main.rb
@@ -27,7 +27,6 @@ def gameOver_and_winner(player)
 end
 
 def question_generator()
-  while true
     def getRandom() 
       rand(1..10)
     end
@@ -44,55 +43,87 @@ def question_generator()
     puts "#{total}"
     print">  press 1 to play 2 exit "
     choice = $stdin.gets.chomp
+    "P1: #{player_1.score}/3 vs #{player_2.score}"
     puts "this is XXXXXX #{mychoice = choice.to_i}"
-  end
 end
 
 
 def player_1
   puts "---- New Turn ----"
 
-  puts "#{player_1} What does #{val1} plus #{val2} equal?"
-  puts "> " # getchomps
+  # puts "#{player_1} What does #{val1} plus #{val2} equal?"
+  puts "> player_1 " # getchomps
+  choice1 = $stdin.gets.chomp
+  choice = choice1.to_i
 
-    if true
-      puts "#{player_1}: YES! You are correct."
-    else
-      puts "#{player_1}: Seriously? No!"
-      player_1.minus_1
-    end
+  #   if true
+  #     puts "#{player_1}: YES! You are correct."
+  #   else
+  #     puts "#{player_1}: Seriously? No!"
+  #     player_1.minus_1
+  #   end
 
-    if player_1.player_score < 1
-      gameOver_and_winner(player)
-    else
-  # show score
-  puts "P1: #{player_1.score}/3 vs #{player_2.score}"
+  #   if player_1.player_score < 1
+  #     gameOver_and_winner(player)
+  #   else
+  # # show score
+  if choice == 1
+    puts "P1: #{player_1.score}/3 vs #{player_2.score}"
+  else
+  # end
+    game_engine
   end
-  player_2
 end
 
 
-def player_2
- 
-  puts "---- New Turn ----"
+# def player_2
+#   puts "---- New Turn ----"
+#   puts "#{player_2} What does #{val1} plus #{val2} equal?"
+#   puts "> " # getchomps
 
-  puts "#{player_2} What does #{val1} plus #{val2} equal?"
-  puts "> " # getchomps
-
-    if true
-      puts "#{player_2}: YES! You are correct."
-    else
-      puts "#{player_2}: Seriously? No!"
-      player_2.minus_1
-    end
+#     if true
+#       puts "#{player_2}: YES! You are correct."
+#     else
+#       puts "#{player_2}: Seriously? No!"
+#       player_2.minus_1
+#     end
     
-    if player_2.player_score < 1
-      gameOver_and_winner(player)
+#     if player_2.player_score < 1
+#       gameOver_and_winner(player)
+#     else
+#   # show score
+#   puts "P1: #{player_1.score}/3 vs P2: #{player_2.score}"
+#   end
+#   game_engine
+# end
+
+def game_engine
+# player_1 = PlayerScore.new()
+# player_2 = PlayerScore.new()
+
+# player_1.player_score == 3
+# player_2.player_score == 3
+
+# player_1.minus_11
+  while true # 
+    puts "---- New Turn ----"
+    puts "> You are here"
+    choice1 = $stdin.gets.chomp
+    choice = choice1.to_i
+    if choice == 1
+      player_1 #player_1.player_score
+      question_generator()
+      # exit(0)
+    elsif choice == 3
+      # puts player_1.player_score 
+    elsif choice == 4
+      # puts player_2.player_score 
     else
-  # show score
-  puts "P1: #{player_1.score}/3 vs P2: #{player_2.score}"
+      puts "I got no idea what that means."
+      # gameOver_and_winner(player_1)
+
+    end
   end
-  player_1
 end
 
 
@@ -100,50 +131,10 @@ end
 def start
   player_1 = PlayerScore.new()
   player_2 = PlayerScore.new()
-  player_1
+  if true
+    game_engine
+  end 
 end
 
 start
 
-
-# def start()
-#   player_1 = PlayerScore.new()
-#   player_2 = PlayerScore.new()
-  
-#   while true
-    
-#     def getRandom() 
-#       rand(1..10)
-#     end
-    
-#     def add(a, b)
-#       a + b
-#     end
-
-#     val_1 = getRandom() 
-#     val_2 = getRandom()
-#     total = add(val_1, val_2)
-    
-    
-#     puts " What does #{val_1} plus #{val_2} equal?"
-#     puts "#{total}"
-#     print">  press 1 to play 2 exit "
-#     choice = $stdin.gets.chomp
-#     puts "this is XXXXXX #{mychoice = choice.to_i}"
-#   end
-    
-#     if mychoice != total
-#       player_1.minus_1
-#       puts "Player One score #{player_1.player_score}"
-#       puts "looping back"
-#       start
-#     end
-#     if mychoice == total
-#       #looping back
-#       puts "looping back"
-#       start
-#     end
-#   end
-# end
-
-start
